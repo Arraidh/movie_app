f605b6518891008d2be74492ed4b1cf827d3fe8f
Arraidh <ari.darmika@gmail.com>
2022-12-03 00:56:56 +0700

    starting over and successfully testing API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect } from 'react';
 
+//74c9900e
 
+const API_url = 'http://www.omdbapi.com?apikey=74c9900e';
 
 const App = () => {
-  const [counter, setCounter] = useState(100);
 
-  useEffect(() => {
-    alert('counter changed ' + counter);
-  }, [counter]);
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_url}&s=${title}`)
+        const data = await response.json();
 
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovie('Spiderman');
+    }, []);
+
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
